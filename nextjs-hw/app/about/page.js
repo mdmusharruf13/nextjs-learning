5f188d8746caf76e65fc163af01f23ed554c9933
musharruf <mdmusharruf13@gmail.com>
2025-02-09 22:58:32 +0530

    using external images

--- nextjs-hw/app/about/page.js
+++ nextjs-hw/app/about/page.js
@@ -1,6 +1,10 @@
+import Image from "next/image"
+
 function page() {
     return (
-        <div>About</div>
+        <section>
+            <Image src={"https://images.unsplash.com/photo-1738924349706-14d70715e236?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="nature image" width={500} height={500} />
+        </section>
     )
 }
 
