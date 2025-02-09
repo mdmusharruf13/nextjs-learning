ceb1b0224cfc8b82039b2517feb80ca437eb60bb
musharruf <mdmusharruf13@gmail.com>
2025-02-09 12:43:28 +0530

    custom not-found page

---
+++ nextjs-hw/app/not-found.js
@@ -0,0 +1,5 @@
+export default function NotFound() {
+    return <section className="flex justify-center items-center min-h-screen">
+        <p className="font-bold text-4xl">404 Not Found</p>
+    </section>
+}
