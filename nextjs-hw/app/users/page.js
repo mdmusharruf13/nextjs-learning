242fd8918b7c945df9d5a637d0807a0e1bc587b0
musharruf <mdmusharruf13@gmail.com>
2025-02-10 00:39:59 +0530

    google fonts

--- nextjs-hw/app/users/page.js
+++ nextjs-hw/app/users/page.js
@@ -1,6 +1,15 @@
+import { Caveat, Indie_Flower, Source_Sans_3 } from "next/font/google";
+
+
+const srcSans = Source_Sans_3({ subsets: ['latin'], weight: "500" })
+const indieFlower = Indie_Flower({ subsets: ['latin'], weight: "400" })
 function Users() {
     return (
-        <div>This is users page</div>
+        <section>
+            <p className={srcSans.className}>This is users page with Roboto Font</p>
+            <p className={`${indieFlower.className} text-2xl`}>This is Indie_Flower font</p>
+
+        </section>
     )
 }
 
