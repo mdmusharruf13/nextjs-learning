e8c4e781162936fb26f890c53d4c5ffffcd995e0
musharruf <mdmusharruf13@gmail.com>
2025-02-11 18:10:04 +0530

    posting data using POST method & use Postman

--- nextjs-hw/app/api/users/route.js
+++ nextjs-hw/app/api/users/route.js
@@ -3,3 +3,13 @@ import { NextResponse } from "next/server";
 export function GET() {
     return NextResponse.json({ success: "this is success message from server", host: process.env.HOST })
 }
+
+export async function POST(req, res) {
+    let { email, password } = await req.json();
+    console.log(email, password);
+    if (!email || !password) {
+        return NextResponse.json({ error: "required field not found" }, { status: 400 })
+    }
+
+    return NextResponse.json({ res: 'Credentials verified, user successfully logged In' });
+}
