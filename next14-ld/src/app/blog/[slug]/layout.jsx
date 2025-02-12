450e1c0206ed55a851a8deee70d43e51cc9b5b2e
musharruf <mdmusharruf13@gmail.com>
2025-02-12 09:59:39 +0530

    layouts in Next.js

---
+++ next14-ld/src/app/blog/[slug]/layout.jsx
@@ -0,0 +1,9 @@
+export default function BlogLayout({ children }) {
+  return (
+    <div>
+      this is blog layout
+      {children}
+      <h2></h2>
+    </div>
+  );
+}
--- next14-ld/src/app/layout.js
+++ next14-ld/src/app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <h2>Main layout</h2>
         {children}
       </body>
     </html>
