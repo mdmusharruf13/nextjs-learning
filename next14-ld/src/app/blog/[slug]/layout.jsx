e573b2e04ead7217f214e2e642cd2f6a5d20a618
musharruf <mdmusharruf13@gmail.com>
2025-02-13 20:44:10 +0530

    single post design

--- next14-ld/src/app/blog/[slug]/layout.jsx
+++ next14-ld/src/app/blog/[slug]/layout.jsx
@@ -1,9 +1,3 @@
 export default function BlogLayout({ children }) {
-  return (
-    <div>
-      this is blog layout
-      {children}
-      <h2></h2>
-    </div>
-  );
+  return <>{children}</>;
 }
--- next14-ld/src/app/blog/[slug]/page.jsx
+++ next14-ld/src/app/blog/[slug]/page.jsx
@@ -1,3 +1,50 @@
-export default function blog() {
-  return <div>blog</div>;
+import styles from "./singlePost.module.css";
+import Image from "next/image";
+
+export default function SinglePostPage() {
+  return (
+    <section className={styles.container}>
+      <section className={styles.imgContainer}>
+        <Image
+          src={"/introImage.png"}
+          alt="post image"
+          width={500}
+          height={500}
+          className={styles.img}
+        />
+      </section>
+      <section className={styles.infoContainer}>
+        <p className={styles.title}>Title</p>
+        <section className={styles.postData}>
+          <section className={styles.avatarContainer}>
+            <Image
+              src={"/contactme.png"}
+              alt="avatar"
+              className={styles.avatar}
+              width={50}
+              height={50}
+            />
+          </section>
+          <section className={styles.info}>
+            <p className={styles.subHeading}>Author</p>
+            <p className={styles.text}>Musharruf</p>
+          </section>
+          <section className={styles.info}>
+            <p className={styles.subHeading}>Published</p>
+            <p className={styles.text}>02 Feb 2024</p>
+          </section>
+        </section>
+        <section className={styles.description}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            at doloremque quisquam perferendis possimus repudiandae illum, eius,
+            laboriosam dicta, et ea fuga debitis in totam aspernatur. Adipisci
+            esse architecto officiis quasi perspiciatis, dolor eligendi
+            cupiditate, quo aperiam, reiciendis veritatis minima impedit ipsa
+            quod nulla accusamus similique aliquid consequatur sunt magni.
+          </p>
+        </section>
+      </section>
+    </section>
+  );
 }
