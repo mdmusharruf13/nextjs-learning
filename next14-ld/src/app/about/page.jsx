9c8ea115e6f81fde2082747b615e2612ce5c255a
musharruf <mdmusharruf13@gmail.com>
2025-02-13 10:42:42 +0530

    next.js image component

--- next14-ld/src/app/about/page.jsx
+++ next14-ld/src/app/about/page.jsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+
 export default function about() {
-  return <div>about</div>;
+  return (
+    <section>
+      <Image
+        src={"/introImage.png"}
+        alt="about image"
+        width={500}
+        height={500}
+      />
+      <Image
+        src={
+          "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnJvbnQlMjBlbmQlMjBkZXZlbG9wZXJ8ZW58MHx8MHx8fDA%3D"
+        }
+        alt="coding image"
+        width={500}
+        height={500}
+      />
+    </section>
+  );
 }
