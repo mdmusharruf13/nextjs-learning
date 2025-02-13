74fbbc7ae0761559f2228c399fd26799f2ebb67b
musharruf <mdmusharruf13@gmail.com>
2025-02-13 14:45:40 +0530

    contact page design

--- next14-ld/src/app/contact/page.jsx
+++ next14-ld/src/app/contact/page.jsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 export default function contact() {
-  return <div>contact</div>;
+  return (
+    <>
+      <section className={styles.container}>
+        <section className={styles.imgContainer}>
+          <Image
+            src={"/contactme.png"}
+            alt="contact me"
+            width={500}
+            height={500}
+          />
+        </section>
+        <section className={styles.formContainer}>
+          <form className={styles.form}>
+            <input type="text" placeholder="Enter Your Name" />
+            <input type="text" placeholder="Email Address" />
+            <input type="text" placeholder="Phone Number (Optional)" />
+            <textarea cols={30} rows={10} placeholder="Message"></textarea>
+            <button className={styles.btn}>Submit</button>
+          </form>
+        </section>
+      </section>
+    </>
+  );
 }
