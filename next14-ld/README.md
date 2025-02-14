871080cb0afd612408604adf420e5059618bebb8
musharruf <mdmusharruf13@gmail.com>
2025-02-15 01:04:36 +0530

    SSR, CSR and Hydration Error

---
+++ next14-ld/README.md
@@ -0,0 +1,41 @@
+## Next.Js
+
+### Rendering
+
+- Server Side Rendering (SSR)
+- Client Side Rendering (CSR)
+
+### SSR
+
+- Pros:
+  - The initial page load is faster.
+  - Better for old devices and slow internet connection.
+  - Better SEO
+- Cons:
+  - Less Interaction.
+  - Increased server loads.
+  - Slower Subsequent page loads.
+  - State Management complexity.
+
+### CSR
+
+- Pros:
+  - Better performance after initia load.
+  - Less server load.
+  - Best for the user interactivity.
+- Cons:
+  - May affect SEO.
+  - Slower initial load.
+  - Dependency on client resources.
+
+### Hydration Error
+
+Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This is happen if a S.S.Rendered client component used.
+
+- A server/client branch `if (typeof window !== 'undefined)`.
+- Variable input such as `Date.now()` or `Math.random()` which changes each time is called.
+- Date formatting in a user's locale which doesn't match the server.
+- External changing data without sending a snapshot of it along with the HTML.
+- Invalid HTML tag nesting.
+
+It can also happen if the client has a browser extension installed which messes with the HTML before React loaded.
--- next14-ld/src/app/page.jsx
+++ next14-ld/src/app/page.jsx
@@ -1,7 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import "../utils/classes.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  let num = Math.random();
+  console.log(num);
+
   return (
     <section className="homeContainer">
       <section className="flex flex-col gap-4">
@@ -10,10 +22,10 @@ export default function Home() {
         </section>
         <section className="flex flex-col gap-2">
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
-            voluptatem et ipsa dicta doloribus sit deserunt. Molestiae porro
-            sequi labore, explicabo accusamus consequatur molestias laudantium
-            mollitia ullam voluptatibus ab commodi.
+            {isClient && num}Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Neque voluptatem et ipsa dicta doloribus sit deserunt.
+            Molestiae porro sequi labore, explicabo accusamus consequatur
+            molestias laudantium mollitia ullam voluptatibus ab commodi.
           </p>
           <section className="flex gap-1">
             <button className="btn-sm bg-blue text-white">Learn More</button>
