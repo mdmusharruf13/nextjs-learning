d70cec469c2d3c6b3509d93270ee1d1798c76baa
musharruf <mdmusharruf13@gmail.com>
2025-03-01 02:03:22 +0530

    fix error

--- nextjs14-sm/src/model/blog.js
+++ nextjs14-sm/src/model/blog.js
@@ -5,6 +5,6 @@ const BlogSchema = new mongoose.Schema({
     description: String
 });
 
-const Blog = mongoose.model.Blog || mongoose.model("Blog", BlogSchema);
+const Blog = mongoose.models.Blog || mongoose.model("Blog", BlogSchema);
 
 export default Blog;
