3325d42607ad7c433455d79b3b8227f451d816a5
musharruf <mdmusharruf13@gmail.com>
2025-03-03 19:25:12 +0530

    util objects

---
+++ nextjs14-sm/src/util/user-helper.js
@@ -0,0 +1,50 @@
+export const initialUserData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+};
+
+export const userFormInputs = [
+    {
+        type: "input",
+        name: "firstName",
+        placeholder: 'Enter first name',
+    },
+    {
+        type: "input",
+        name: "lastName",
+        placeholder: 'Enter last name',
+    },
+    {
+        type: "email",
+        name: "email",
+        placeholder: 'Enter your email',
+    },
+    {
+        type: "input",
+        name: "firstname",
+        placeholder: 'Enter your address',
+    }
+];
+
+export const initialModelInfo = {
+    title: '',
+    btnLabel: ''
+};
+
+export const MODEL_ACTION = {
+    ADD: 'add',
+    UPDATE: 'update'
+};
+
+export const MODEL_STATE = {
+    ADD: {
+        title: "Add New User",
+        btnLabel: "Add User"
+    },
+    UPDATE: {
+        title: "Update User",
+        btnLabel: "Save Changes"
+    }
+}
