ece401e7ac9add05dee85e978e64da3c89f0d1f4
musharruf <mdmusharruf13@gmail.com>
2025-03-04 19:45:12 +0530

    added label property - form input

--- nextjs14-sm/src/util/user-helper.js
+++ nextjs14-sm/src/util/user-helper.js
@@ -10,21 +10,25 @@ export const userFormInputs = [
         type: "input",
         name: "firstName",
         placeholder: 'Enter first name',
+        label: "First Name"
     },
     {
         type: "input",
         name: "lastName",
         placeholder: 'Enter last name',
+        label: "Last Name"
     },
     {
         type: "email",
         name: "email",
         placeholder: 'Enter your email',
+        label: "Email"
     },
     {
         type: "input",
         name: "address",
         placeholder: 'Enter your address',
+        label: "Address"
     }
 ];
 
