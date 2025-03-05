698afd4d75102264a0340c5b0733edfebbaaa720
musharruf <mdmusharruf13@gmail.com>
2025-03-05 07:32:48 +0530

    component removed

--- nextjs14-sm/src/components/Input.js
+++
@@ -1,5 +0,0 @@
-export default function Input({ type, placeholder, name }) {
-    return <>
-        <input type={type} placeholder={placeholder} id={name} required className="border border-gray-400 rounded-md py-2 px-3 " />
-    </>
-}
