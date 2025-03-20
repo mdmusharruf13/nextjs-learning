a2933fbcb439eb61a6b8649f8e16eb69e8a00169
musharruf <mdmusharruf13@gmail.com>
2025-03-20 14:39:45 +0530

    redirecting auth page on cookies present

---
+++ nextjs14-sm/src/middleware.js
@@ -0,0 +1,28 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+    const path = request.nextUrl.pathname;
+    const checkPublicPath = path === '/project-list/auth-user/sign-in' || path === '/project-list/auth-user/sign-up';
+
+    // const cookieStore = await cookies();
+    // const token = cookieStore.get("token")?.value || "";
+
+    const token = request.cookies.get('token')?.value || '';
+    console.log("middleware token: ", token);
+
+
+    if (checkPublicPath && token !== '') {
+        return NextResponse.redirect(new URL("/project-list/auth-user", request.nextUrl));
+    }
+
+    if (!checkPublicPath && token === '') {
+        return NextResponse.redirect(new URL("/project-list/auth-user/sign-up", request.nextUrl));
+    }
+}
+
+export const config = {
+    matcher: [
+        "/project-list/auth-user/sign-in",
+    ]
+};
